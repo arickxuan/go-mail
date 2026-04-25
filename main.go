647130cabce586c97e4df822f9728d0c90ca8b85
V0027028 <V0027028@tal.com>
2026-04-26 02:40:53 +0800

    fix

--- main.go
+++ main.go
@@ -34,6 +34,16 @@ func main() {
 
 	r := gin.Default()
 
+	r.Use(func(c *gin.Context) {
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		c.Header("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		if c.Request.Method == http.MethodOptions {
+			c.AbortWithStatus(http.StatusNoContent)
+			return
+		}
+	})
+
 	h := handlers.New(s)
 	api := r.Group("/api")
 	h.RegisterAPIRoutes(api)
